74212cbb45d2cd92fc858f28da542469e3b5bdf7
Ayoub-Zerdoum <71574216+Ayoub-Zerdoum@users.noreply.github.com>
2024-05-28 13:42:21 +0100

    adding the cv download feature and contact infos

--- Scripts/contactHexStyle.js
+++ Scripts/contactHexStyle.js
@@ -4,6 +4,23 @@ function toggleHexContainer() {
     hexContainer.classList.toggle('open');
     niceDay.classList.toggle('open');
 }
+
+function afficherContact(contact) {
+    const gmailInfo = document.getElementById('gmail');
+    const telephoneInfo = document.getElementById('telephone');
+    console.log("in afficher contact fonction");
+    if(contact == "gmail"){
+        gmailInfo.classList.add('open');
+        telephoneInfo.classList.remove('open');
+        console.log("showing gmail");
+    }
+
+    if(contact == "telephone"){
+        gmailInfo.classList.remove('open');
+        telephoneInfo.classList.add('open');
+        console.log("showing gmail");
+    }
+}
 /*
 function closeHexContainer() {
     const hexContainer = document.querySelector('.contact-hex-container');
